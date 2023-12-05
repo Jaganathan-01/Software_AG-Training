3b906e614d3707d319c7ece08a2ca30b44359207
Jaganathan Govindhan <100895340+Jaganathan-01@users.noreply.github.com>
2023-12-05 16:12:42 +0530

    Create update-readme.sh
    script file push code into master

---
+++ update-readme.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ 
+# Ensure we are in the repository root directory
+cd "$(git rev-parse --show-toplevel)"
+ 
+# Get the pull request title and description
+pr_title=$(git show --format=%s -s HEAD)
+pr_description=$(git log --format=%b -n 1)
+ 
+# Create the output directory if it doesn't exist
+mkdir -p output
+ 
+# Check if the description is not empty before appending to the readme.txt file
+if [ -n "$pr_description" ]; then
+  echo "### Pull Request Title: $pr_title" >> output/readme.txt
+  echo "**Description:**" >> output/readme.txt
+  echo "$pr_description" >> output/readme.txt
+  echo "=========================" >> output/readme.txt
+else
+  echo "### Pull Request Title: $pr_title" >> output/readme.txt
+  echo "=========================" >> output/readme.txt
+fi
